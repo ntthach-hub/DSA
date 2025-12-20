354f2e8b01f00420017fde9387ce779ca96cfb49
ntthach-hub <thachngocthao05@gmail.com>
2025-12-20 08:16:28 +0700

    dont use <algorithm>

--- sorting algorithms/Selection_sort/ex2_desc_selection.cpp
+++ sorting algorithms/Selection_sort/ex2_desc_selection.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <iomanip>    // setw
-#include <algorithm>  // max_element, swap
-#include <cmath>
 using namespace std;
 
 void nhap_mang(int a[], int &n) {
@@ -19,6 +17,12 @@ void xuat_mang(int a[], int n) {
     for (int i = 0; i < n; i++)
         cout << setw(4) << a[i];  // in cach deu moi phan tu
     cout << endl;
+}
+
+void swap(int &x,int &y){
+	int temp = x;
+	x = y;
+	y = temp;
 }
 void Selection_sort(int a[], int n)
 {
